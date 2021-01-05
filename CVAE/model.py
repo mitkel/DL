e60110e81ce170f2c7907ed26d0f9621a9f97783
mitkel <>
2021-01-05 16:39:43 +0100

    debugged model

--- CVAE/model.py
+++ CVAE/model.py
@@ -1,28 +1,32 @@
 import torch
-import torch.nn.functional as nn
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
 
 class CVAE(nn.Module):
     def __init__(self,
                  input_dim: int,
                  cond_dim: int,
                  latent_dim: int,
+                 reg: Tensor = torch.ones(1),
                  bias: bool = True,
-                 layers: List = None,
+                 layers: list = None,
+                 device: torch.device = torch.device("cpu"),
                  **kwargs) -> None:
         super().__init__()
 
+        self.device = device
         self.latent_dim = latent_dim
+        self.reg = reg.to(self.device)
 
         # encoder
-        in_features = input_dim + cond_dim
         modules = []
-
+        in_features = input_dim + cond_dim
         for h_dim in layers:
             modules.append(
-                nn.Linear(in_features=in_features,
-                          out_features=h_dim,
-                          bias=bias),
-                nn.ReLU,
+                nn.Sequential(
+                    nn.Linear(in_features, h_dim, bias),
+                    nn.ReLU()),
             )
             in_features = h_dim
 
@@ -31,55 +35,83 @@ class CVAE(nn.Module):
         self.fc_mu = nn.Linear(layers[-1], latent_dim)
         self.fc_logvar = nn.Linear(layers[-1], latent_dim)
 
-
+        #  decoder
         modules = []
         in_features = latent_dim + cond_dim
-        for h_dim in list(reversed(layers)):
+        for h_dim in reversed(layers):
             modules.append(
-                nn.Linear(in_features=in_features,
-                          out_features=h_dim,
-                          bias=bias),
-                nn.ReLU,
-            )
+                nn.Sequential(
+                    nn.Linear(in_features, h_dim, bias),
+                    nn.ReLU()
+            ))
             in_features = h_dim
 
-        modules.append( # final layers is linear
-            nn.Linear(in_features=in_features,
-                      out_features=input_dim,
-                      bias=bias)
+        modules.append( # final layer is linear
+            nn.Linear(in_features, input_dim, bias)
         )
         self.decoder = nn.Sequential(*modules)
 
-    def encode(self, input: Tensor) -> List(Tensor):
+    def encode(self, input: Tensor, cond: Tensor) -> list:
         """
-        encodes the input and returns code pair [mu, logvar]
+        encodes the input and returns the code pair [mu, logvar]
         """
-        code = self.encoder(input)
+        try:
+            x = torch.cat([input, cond], dim=1)
+        except:
+            if len(input.shape) == 1:
+                input = input.unsqueeze(1)
+            if len(cond.shape) == 1:
+                cond = cond.unsqueeze(1)
+            x = torch.cat([input, cond], dim=1)
+        code = self.encoder(x)
         mu = self.fc_mu(code)
         logvar = self.fc_logvar(code)
         return [mu, logvar]
 
-    def decode(self, z: Tensor, cond: Tensor) -> Tensor:
-        x = torch.cat([z, cond])
-        return self.decoder(x)
-
     def sample_latent(self, mu:Tensor, logvar:Tensor) -> Tensor:
         """
-        given code pair returns latent variable
+        given the code pair returns latent rv. z
         """
         sdev = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(sdev)
-        return mu + eps * sdev
+        eps = torch.randn_like(sdev, device = self.device)
+        return (mu + eps * sdev)
 
-    def forward(self, input:Tensor, cond:Tensor) -> List(Tensor):
-        x = torch.cat([input, cond], dim=1)
-        [mu, logvar] = self.encode(x)
+    def decode(self, z: Tensor, cond: Tensor) -> Tensor:
+        """
+        decodes the latent rv. z given conditional knowledge
+        """
+        try:
+            x = torch.cat([z, cond], dim=1)
+        except:
+            if len(z.shape) == 1:
+                z = z.unsqueeze(1)
+            if len(cond.shape) == 1:
+                cond = cond.unsqueeze(1)
+            x = torch.cat([z,cond], dim=1)
+        return self.decoder(x)
+
+    def forward(self, input:Tensor, cond:Tensor) -> list:
+        [mu, logvar] = self.encode(input, cond)
         z = self.sample_latent(mu, logvar)
-        return [self.decode(z), mu, logvar]
+        return [self.decode(z, cond), mu, logvar]
+
+    def loss_func(self,
+                  pred: Tensor,
+                  input: Tensor,
+                  mu: Tensor,
+                  logvar: Tensor) -> list:
+        MSE = F.mse_loss(pred, input)
+        try:
+            KLD = torch.mean(-0.5 * torch.sum(1+logvar - mu.pow(2) - logvar.exp(), dim=1))
+        except:
+            KLD = torch.mean(-0.5 * 1+logvar - mu.pow(2) - logvar.exp() )
+
+        return [MSE*self.reg + KLD, MSE, KLD]
 
     def sample_trajectories(self,
                             num_samples: int,
-                            cond: Tensor) -> List(Tensor):
+                            cond: Tensor) -> list   :
 
         z = torch.randn(num_samples, self.latent_dim)
-        return [self.decode(z[i,:], cond) for i in range(num_samples)]
+        c = torch.stack([cond for _ in range(num_samples)], dim=0)
+        return self.decode(z, c).detach()
---
+++ CVAE/train.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+
+from model import CVAE
+from parameters import params
+from gen_data import OU
+
+def train(model, train_loader, device, verbose=False, **kwargs):
+    optimizer = kwargs['optimizer'](model.parameters(), lr=kwargs['lr'])
+    model.train()
+
+    for epoch in range(kwargs['epochs']):
+        loss = torch.zeros((), device = device)
+        MSE = torch.zeros((), device = device)
+        KLD = torch.zeros((), device = device)
+
+        for cond, inpt in train_loader:
+            cond = cond.to(device)
+            inpt = inpt.to(device)
+            optimizer.zero_grad()
+
+            outputs, mu, logvar = model(inpt, cond)
+            train_loss, MSE_train, KLD_train = model.loss_func(outputs, inpt, mu, logvar)
+            train_loss.backward()
+            optimizer.step()
+            loss += train_loss.item()
+            MSE += MSE_train.item()
+            KLD += KLD_train.item()
+
+        loss = loss / len(train_loader)
+        MSE = MSE / len(train_loader)
+        KLD = KLD / len(train_loader)
+        if verbose:
+            print("{}: L {:.5f} MSE {:.5f} KLD {:.5f}".format(epoch + 1,
+                                                              loss.item(),
+                                                              MSE.item(),
+                                                              KLD.item()))
+    return [mu.to(torch.device("cpu")).detach(), logvar.to(torch.device("cpu")).detach()]
+
+def main(sample_name):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    set0 = OU(np.load("data/" + sample_name + ".npy"), M=params['M'])
+    loader = DataLoader(set0, params['batch_size'])
+    model = CVAE(device=device, **params).to(device)
+    mu, logvar = train(model, loader, device, True, **params)
+    name = params['name']+"-"+sample_name
+    torch.save(model.state_dict(),name+"_model")
+    np.save(name+"_mu",mu.numpy())
+    np.save(name+"_logvar",logvar.numpy())
+
+
+if __name__ == '__main__':
+    main("train1")
