f1d70f9cbd019ff1fe95a27c7ef165e53cea2f1f
mitkel <>
2021-01-07 02:12:53 +0100

    removed obsolete parameters M,T

--- CVAE/parameters.py
+++ CVAE/parameters.py
@@ -6,21 +6,21 @@ params = {
     'mu': 1.,
     'sigma': 0.2,
     'theta': 1.,
-    'input_dim': 30,
-    'cond_dim': 330,
+    'input_dim': 100,
+    'cond_dim': 10,
 
     # samples
     'train_samples': 10000,
-    'test_samples': 1000,
     'slices': 10,
 
     # model
     'latent_dim': 50,
     'bias': False,
     'layers': [1000,500,100],
+    'end_relu': False,
 
     # training
-    'epochs': 10,
+    'epochs': 100,
     'batch_size': 100,
     "optimizer": optim.Adam,
     "lr": 1e-3,
