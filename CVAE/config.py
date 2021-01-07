d8dfdb71c1fbb4da6842519bf91461c9ecae9d8c
mitkel <>
2021-01-07 02:10:44 +0100

    renamed file

---
+++ CVAE/config.py
@@ -0,0 +1,31 @@
+import torch
+import torch.optim as optim
+
+params = {
+    # data
+    'mu': 1.,
+    'sigma': 0.2,
+    'theta': 1.,
+    'input_dim': 100,
+    'cond_dim': 10,
+
+    # samples
+    'train_samples': 100000,
+    'slices': 10,
+
+    # model
+    'latent_dim': 50,
+    'bias': False,
+    'layers': [1000,500,100],
+
+    # training
+    'epochs': 100,
+    'batch_size': 1000,
+    "optimizer": optim.Adam,
+    "lr": 1e-3,
+}
+
+params.update({
+    'name': f"res/CVAE-L{params['layers']}-C{params['latent_dim']}",
+    'reg': torch.ones(1),
+})
